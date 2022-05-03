ea2529a1ba40eafb63a0c9eeb210673555518ce9
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-03 22:57:44 +0100

    steppingstones-server: modified  cors option server.js

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
-const allowedOrigins = ["http://localhost:3000"];
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
-}
+// const allowedOrigins = ["http://localhost:3000"];
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins,
+// }
 
-app.use(cors(options));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
