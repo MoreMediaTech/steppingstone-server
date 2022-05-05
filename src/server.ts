06b60b16f783a42de6d75fc0daa463e6d1378a00
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-05 17:02:46 +0100

    steppingstones-server: updated cors options

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,27 @@ import { router as userRoutes } from "./routes/userRoutes";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
-// const allowedOrigins = ["http://localhost:3000"];
-// const options: cors.CorsOptions = {
-//   origin: allowedOrigins,
-// }
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://steppingstonesapp.com/",
+];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+    "Authorization",
+  ],
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-app.use(cors());
+app.use(cors(options));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -39,7 +52,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
 
