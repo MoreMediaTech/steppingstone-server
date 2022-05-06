290af690146f4f2af9ba6a5b64df389521cb1aa5
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-06 14:47:07 +0100

    steppingstones-server: updated serverjs. modified  userController and server.ts

--- src/server.ts
+++ src/server.ts
@@ -15,18 +15,9 @@ const allowedOrigins = [
 ];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-  allowedHeaders: [
-    "Content-Type",
-    "authorization",
-    "Content-Length",
-    "X-Requested-With",
-    "Accept",
-  ],
-  credentials: true,
 };
 
-app.use(cors());
+app.use(cors(options));
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
