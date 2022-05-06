1000c193cc4ca4eee6a7347be6d27e96eb69cb49
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-05-06 13:52:15 +0100

    steppingstones-server: updated serverjs. modified  userController

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ const options: cors.CorsOptions = {
   credentials: true,
 };
 
-app.use(cors(options));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
