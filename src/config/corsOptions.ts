a337f8edc5dd464b2aefcbda3cb5d3f4c6ee2c8b
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-06-08 12:27:43 +0100

    steppingstones-server: updated corsOptions

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,13 +1,14 @@
 import { allowedOrigins } from "./allowedOrigins";
 
 export const corsOptions = {
-    origin: (origin: any, callback: any) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    // origin: (origin: any, callback: any) => {
+    //     if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // },
+    origin: allowedOrigins,
     optionsSuccessStatus: 200,
     credentials: true
 }
